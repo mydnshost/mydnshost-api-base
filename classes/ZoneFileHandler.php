0f14dcdf95804a58a09f330f330d00df6bb0721b
Shane Mc Cormack <shanemcc@gmail.com>
2021-10-27 02:22:24 +0100

    Split RecordsInfo to separate file.

--- classes/ZoneFileHandler.php
+++ classes/ZoneFileHandler.php
@@ -1,5 +1,4 @@
 <?php
-
 	abstract class ZoneFileHandler {
 		/**
 		 * Take a zone file, and return parsed data.
@@ -36,66 +35,5 @@
 			} else {
 				throw new Exception('Unknown zone file type: ' . $type);
 			}
-		}
-	}
-
-
-	class RecordsInfo {
-		private $records = [];
-
-		public function removeRecords($rrname, $rrtype) {
-			if (!isset($this->records[$rrtype])) { return; }
-			if (!isset($this->records[$rrtype][$rrname])) { return; }
-
-			unset($this->records[$rrtype][$rrname]);
-		}
-
-		public function addRecord($rrname, $rrtype, $content, $ttl, $priority = null) {
-			if (!isset($this->records[$rrtype])) { $this->records[$rrtype] = []; }
-			if (!isset($this->records[$rrtype][$rrname])) { $this->records[$rrtype][$rrname] = []; }
-
-			$value = ['Address' => $content, 'TTL' => $ttl];
-			if ($priority !== null) { $value['Priority'] = $priority; }
-
-			$this->records[$rrtype][$rrname][] = $value;
-		}
-
-		public function mergeFrom(RecordsInfo $recordsInfo) {
-			foreach ($recordsInfo->records as $rrtype => $entries) {
-				foreach ($entries as $rrname => $records) {
-					if (!isset($this->records[$rrtype])) { $this->records[$rrtype] = []; }
-					if (!isset($this->records[$rrtype][$rrname])) { $this->records[$rrtype][$rrname] = []; }
-
-					foreach ($records as $record) {
-						$this->records[$rrtype][$rrname][] = $record;
-					}
-				}
-			}
-		}
-
-		public function getByName($name) {
-			$result = [];
-			foreach ($this->records as $type => $entries) {
-				foreach ($entries as $rname => $records) {
-					if ($rname != $name) { continue; }
-					foreach ($records as $record) {
-						$result[] = ['Name' => $rname,
-						             'Type' => $type,
-						             'Address' => $record['Address'],
-						             'TTL' => $record['TTL'],
-						             'Priority' => isset($record['Priority']) ? $record['Priority'] : null,
-						            ];
-					}
-				}
-			}
-			return $result;
-		}
-
-		public function get() {
-			return $this->records;
-		}
-
-		public function clear() {
-			$this->records = [];
 		}
 	}
